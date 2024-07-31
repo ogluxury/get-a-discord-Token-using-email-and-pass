46cced6f4f8d9e2ac20477b0befe3fbcdd49c42f
ogluxury <dfwdwdwdwefw@gmail.com>
2024-07-31 22:46:20 +0300

    Update token.js

--- token.js
+++ token.js
@@ -1,17 +1,17 @@
 const axios = require('axios');
 
 async function gettoken() {
-let email = 'discord email';
-let pass = 'discord pass';
+let email = 'discord email'
+let pass = 'discord pass'
 
 let payload = {
   login: email,
   password: pass
-};
+}
 
 let response = await axios.post('https://discord.com/api/v9/auth/login', payload)
 console.log(response.data.token)
 console.log('made by ogluxury')
-}
+};
 
 gettoken()
